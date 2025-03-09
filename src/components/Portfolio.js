99fb63f2a7b70927756952b2bdedd17c04e7455d
bimbleboi <kempfa7@gmail.com>
2025-03-09 01:27:51 -0800

    fixingmobilething

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import StarBackground from './StarBackground';
 import '../styles/Portfolio.css';
-import { triggerHaptic } from '../utils/haptics';
 
 const Portfolio = () => {
     const navigate = useNavigate();
 
-    const handleButtonPress = (path) => {
-        triggerHaptic();
-        navigate(path);
-    };
-
     return (
         <div className="portfolio-container">
-            <StarBackground />
             <div className="menu-container">
                 <div className="menu-items">
                     <button 
                         className="menu-item"
-                        onClick={() => handleButtonPress('/music')}
-                        onTouchStart={() => triggerHaptic()}
-                        onTouchEnd={(e) => e.preventDefault()}
+                        onClick={() => navigate('/music')}
                     >
                         Streaming
                     </button>
                     <button 
                         className="menu-item"
-                        onClick={() => handleButtonPress('/videos')}
-                        onTouchStart={() => triggerHaptic()}
-                        onTouchEnd={(e) => e.preventDefault()}
+                        onClick={() => navigate('/videos')}
                     >
                         Visuals
                     </button>
