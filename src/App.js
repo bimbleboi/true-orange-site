27c8013018567c286a838c424f01a7fe88b2bf8f
bimbleboi <kempfa7@gmail.com>
2026-05-03 22:34:19 -0700

    trueorangesite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import Portfolio from './components/Portfolio';
-import GraffitiGallery from './components/GraffitiGallery';
-import Clothing from './components/Clothing';
+import TrueOrangeLanding from './components/TrueOrangeLanding';
 import './styles/App.css';
-import './styles/StarBackground.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/videos" element={<GraffitiGallery />} />
-        <Route path="/music" element={<Clothing />} />
-      </Routes>
-    </Router>
-  );
+  return <TrueOrangeLanding />;
 }
 
-export default App; 
+export default App;
